855d9d030b8b6feced0a55c67f06cb8b89e90768
tomast1337 <vycasnicolas@gmail.com>
2021-12-14 16:42:50 -0300

    Melhorias em cep.py

--- cep.py
+++ cep.py
@@ -2,7 +2,7 @@ import struct
 import sys
 
 if len(sys.argv) != 2:
-    print("USO python3 {} [CEP]".format(sys.argv[0]))
+    print("USO python3 {} [NOME]".format(sys.argv[0]))
     quit()
 
 registroCEP = struct.Struct("72s72s72s72s2s8s2s")
@@ -15,10 +15,9 @@ with open("cep.dat", "rb") as f:
     while len(line) > 0:
         record = registroCEP.unpack(line)
         if sys.argv[1].upper() in record[NomeColumn].decode('latin1'):
+            counter += 1
             for i in range(len(record)):
                 print(record[i].decode('latin1'))
         line = f.read(registroCEP.size)
-        counter += 1
-    print("Total de Registros Lidos: {}".format(counter))
 
-
+    print(f"total de registros coincidindo: {counter}")
